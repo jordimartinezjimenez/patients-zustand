cfa4ccfa0b3b0eac5fa1a740218aefbd8c952b8f
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-09-19 22:53:44 +0200

    Patient delete function

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,11 +1,15 @@
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store"
 
 type PatientDetailsProps = {
     patient: Patient
 }
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+
+    const { deletePatient } = usePatientStore()
+
     return (
         <div className="px-5 py-10 mx-5 my-10 bg-white shadow-md rounded-xl">
             <PatientDetailItem label="ID" data={patient.id} />
@@ -23,8 +27,9 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
                 </button>
                 <button
                     type="button"
-                    className="px-10 py-2 font-bold text-white uppercase rounded-lg bg-rose-600 hover:bg-rose-700">
-                    Eliminar
+                    className="px-10 py-2 font-bold text-white uppercase rounded-lg bg-rose-600 hover:bg-rose-700"
+                    onClick={() => deletePatient(patient.id)}
+                >Eliminar
                 </button>
             </div>
         </div>
